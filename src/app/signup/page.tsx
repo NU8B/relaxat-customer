42dc84bcc7009fd2490a5e6b62c3ed93a7c26ede
NU8B <nonoforges@gmail.com>
2024-03-02 23:42:12 +0700

    main

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,6 +7,7 @@ export default function SignupPage() {
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    console.log("Form submitted!");
     const isEmailValid = validateEmail();
     const isPasswordValid = validatePassword();
 
@@ -144,7 +145,7 @@ export default function SignupPage() {
                 <input
                   id="firstname"
                   name="firstname"
-                  type="firstname"
+                  type="text"
                   autoComplete="current-firstname"
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
